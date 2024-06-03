a88ba3b14a2e3d17c99a970a91aff9bf7c9e34dc
Luis Mindiola <lmindiola89@gmail.com>
2024-06-03 18:19:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function App() {
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark )").matches) {
+      return "dark";
+    }
+    return "light";
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("html").classList.add("dark");
+    } else {
+      document.querySelector("html").classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    console.log(theme);
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center dark:bg-neutral-900">
+      <button
+        className="bg-slate-200 px-4 py-2 rounded hover:bg-slate-300 dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900"
+        onClick={handleChangeTheme}
+      >
+        Change Theme
+      </button>
+    </div>
+  );
+}
+
+export default App;
